f99f2acd8cbe53e929bd49ba6ed60ada247f52a2
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2022-08-20 11:27:17 +1200

    WIP

--- src/Extensions/ElementalTemplatePageExtension.php
+++ src/Extensions/ElementalTemplatePageExtension.php
@@ -14,6 +14,7 @@ class ElementalTemplateExtension extends DataExtension
      */
     protected function onAfterWrite()
     {
+        $owner = $owner->getOwner();
         if($this->elementalTemplateInfiniteLoopCheck === false) {
             $this->elementalTemplateInfiniteLoopCheck = true;
             $owner->findOrMakeDefaultElements();
